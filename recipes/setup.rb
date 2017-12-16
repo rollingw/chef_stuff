6a3afd856742be8309b5e333b0c729ae7fcf77b5
rollingw <spsweet123@gmail.com>
2017-12-16 21:32:33 +0000

    workstation cookbook initial

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'ntp' do
+  action :install
+end
+
+package 'tree' do
+  action :install
+end
+
+file '/etc/mot' do
+  owner 'root'
+  group 'root'
+  content 'This server is the property of Patrick Gibbons'
+end
+
+package 'git' do
+  action :install
+end
+
+service 'ntpd' do
+  action [:enable, :start]
+end
